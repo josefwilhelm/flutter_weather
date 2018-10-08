ba3a0d3d503cc5b2ab0f921ebfc88b091d9bbc81
Josef Wilhelm <3feetsmaller88@gmail.com>
2018-10-08 21:30:40 +0200

    refactor widgets

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:flutter/cupertino.dart'; //new
 
 class Login extends StatefulWidget {
@@ -13,40 +12,28 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: 
-           Container(
-            padding: const EdgeInsets.all(28.0),
-            
-            child: Column(
-                // crossAxisAlignment: CrossAxisAlignment.stretch,
-                // mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 60.0,
-                  ),
-                  Text(
-                    "KITTY MINGSI",
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 44.0,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 4.0,
-                  ),
-                  Expanded(
-                    child: Icon(
-                      IconData(0xe3ea, fontFamily: 'MaterialIcons'),
-                      size: 120.0,
-                      color: Colors.blue,
-                    ),
-                    flex: 1,
-                  ),
-                  Expanded(flex: 3, child: LoginForm()),
-                ]),
-          ),
-        );
+      // backgroundColor: const Color(0xFFF7EBE8),
+      body: Container(
+        padding: const EdgeInsets.all(28.0),
+        child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 60.0,
+              ),
+              Expanded(
+                child: Icon(
+                  IconData(0xe3ea, fontFamily: 'MaterialIcons'),
+                  size: 120.0,
+                  color: Theme.of(context).primaryColor,
+                ),
+                flex: 1,
+              ),
+              Expanded(flex: 3, child: LoginForm()),
+            ]),
+      ),
+    );
   }
 }
 
@@ -73,7 +60,7 @@ class _LoginFormState extends State<LoginForm> {
           labelStyle: TextStyle(fontSize: 18.0),
           icon: Icon(
             FontAwesomeIcons.envelope,
-            color: Colors.blue,
+            color: Theme.of(context).accentColor,
           ),
           labelText: "E-mail address",
           border: UnderlineInputBorder(
@@ -94,7 +81,7 @@ class _LoginFormState extends State<LoginForm> {
       decoration: InputDecoration(
           icon: Icon(
             FontAwesomeIcons.lock,
-            color: Colors.blue,
+            color: Theme.of(context).accentColor,
           ),
           labelText: "Password",
           hintText: "****",
@@ -106,15 +93,16 @@ class _LoginFormState extends State<LoginForm> {
               )),
           border: UnderlineInputBorder(
             borderSide: BorderSide(
-                color: Colors.blue, width: 1.0, style: BorderStyle.solid),
+                color: Theme.of(context).accentColor,
+                width: 1.0,
+                style: BorderStyle.solid),
           )),
     );
 
-    final loginButton = Container( 
+    final loginButton = Container(
         child: Theme.of(context).platform == TargetPlatform.iOS
             ? CupertinoButton(
-                color: Colors.blue,
-                disabledColor: Colors.yellow,
+                color: Theme.of(context).accentColor,
                 onPressed: () {
                   Scaffold.of(context).showSnackBar(
                       SnackBar(content: Text('Submit button pressed')));
@@ -122,25 +110,26 @@ class _LoginFormState extends State<LoginForm> {
                 child: Container(
                     child: Text(
                   'Submit',
-                  style: TextStyle(color: Colors.white, fontSize: 22.0),
                 )))
-            : MaterialButton(
-                color: Colors.blue,
+            : RaisedButton(
+                color: Theme.of(context).primaryColor,
                 onPressed: () {
-                  Scaffold.of(context).showSnackBar(
-                      SnackBar(content: Text('Submit button pressed')));
+                  Navigator.pushReplacementNamed(context, "/home");
                 },
                 child: Container(
+                    padding: const EdgeInsets.all(12.0),
                     child: Text(
-                  'Submit',
-                  style: TextStyle(color: Colors.white, fontSize: 22.0),
-                ))));
+                      'Submit',
+                      style: TextStyle(fontSize: 18.0, color: Colors.white),
+                    ))));
 
     return Container(
-        child: ListView(children: [
+        child: Column(children: [
       Text("Login",
           style: TextStyle(
-              color: Colors.blue, fontSize: 22.0, fontWeight: FontWeight.bold)),
+              color: Theme.of(context).primaryColor,
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold)),
       SizedBox(height: 16.0),
       email,
       SizedBox(height: 16.0),
@@ -155,7 +144,7 @@ class _LoginFormState extends State<LoginForm> {
       Container(
           child: Theme.of(context).platform == TargetPlatform.iOS
               ? CupertinoButton(
-                  color: Colors.white,
+                  color: Theme.of(context).primaryColor,
                   onPressed: () {
                     Scaffold.of(context).showSnackBar(
                         SnackBar(content: Text('Register button pressed')));
@@ -163,10 +152,10 @@ class _LoginFormState extends State<LoginForm> {
                   child: Container(
                       child: Text(
                     'Register',
-                    style: TextStyle(color: Colors.blue, fontSize: 18.0),
+                    style: TextStyle(
+                        color: Theme.of(context).accentColor, fontSize: 18.0),
                   )))
               : FlatButton(
-                  color: Colors.white,
                   onPressed: () {
                     Scaffold.of(context).showSnackBar(
                         SnackBar(content: Text('Register button pressed')));
@@ -174,7 +163,8 @@ class _LoginFormState extends State<LoginForm> {
                   child: Container(
                       child: Text(
                     'Register',
-                    style: TextStyle(color: Colors.blue, fontSize: 18.0),
+                    style: TextStyle(
+                        color: Theme.of(context).accentColor, fontSize: 18.0),
                   ))))
     ]));
   }
