cb1102412a15e25a30feb83aaa6fdcb3272bebed
Josef Wilhelm <3feetsmaller88@gmail.com>
2018-10-10 22:18:54 +0200

    charts yoyoyoooo

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
             textTheme: Theme.of(context)
                 .textTheme
                 .apply(bodyColor: const Color(0xFF53687E))),
-        initialRoute: "/",
+        initialRoute: "/home",
         routes: {
           "/": (context) => Login(),
           "/home": (context) => MyHomePage()
@@ -52,17 +52,44 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text("Kitty Mingsi"), //new Text(widget.title),
-      ),
-      body: new ListView(children: <Widget>[
-        Text('Hello and welcome'),
-        AreaAndLineChart.withSampleData()
-      ]),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        appBar: new AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: new Text("Look at my charts"), //new Text(widget.title),
+        ),
+        body: Builder(
+            builder: (context) => Column(
+                  children: <Widget>[
+                    Text(
+                      "Yo headline",
+                      style: Theme.of(context).textTheme.display2,
+                    ),
+                    Expanded(
+                      child: ListView(
+                          // padding: EdgeInsets.all(24.0),
+                          children: <Widget>[
+                            RaisedButton(
+                                child: Text("Click me"),
+                                onPressed: () {
+                                  Scaffold.of(context).showSnackBar(
+                                      SnackBar(content: Text('Halloooooooo')));
+                                }),
+                            Container(
+                                height: 300.0,
+                                child: AreaAndLineChart.withSampleData()),
+                            RaisedButton(
+                                child: Text("Click me"),
+                                onPressed: () {
+                                  Scaffold.of(context).showSnackBar(
+                                      SnackBar(content: Text('Halloooooooo')));
+                                }),
+                            Container(
+                                height: 300.0,
+                                child: AreaAndLineChart.withSampleData()),
+                          ]),
+                    ),
+                  ],
+                )));
   }
 }
 
@@ -83,58 +110,45 @@ class AreaAndLineChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new charts.LineChart(seriesList,
-        animate: animate,
-        customSeriesRenderers: [
-          new charts.LineRendererConfig(
-              // ID used to link series to this renderer.
-              customRendererId: 'customArea',
-              includeArea: true,
-              stacked: true),
-        ]);
+    return new charts.TimeSeriesChart(
+      seriesList,
+      animate: true,
+      // Optionally pass in a [DateTimeFactory] used by the chart. The factory
+      // should create the same type of [DateTime] as the data provided. If none
+      // specified, the default creates local date time.
+      // dateTimeFactory: const charts.LocalDateTimeFactory(),
+    );
   }
 
   /// Create one series with sample hard coded data.
-  static List<charts.Series<LinearSales, int>> _createSampleData() {
-    final myFakeDesktopData = [
-      new LinearSales(0, 5),
-      new LinearSales(1, 25),
-      new LinearSales(2, 100),
-      new LinearSales(3, 75),
-    ];
-
-    var myFakeTabletData = [
-      new LinearSales(0, 10),
-      new LinearSales(1, 50),
-      new LinearSales(2, 200),
-      new LinearSales(3, 150),
+  static List<charts.Series<WeatherValue, DateTime>> _createSampleData() {
+    final data = [
+      new WeatherValue(new DateTime(2017, 9, 10), 5),
+      new WeatherValue(new DateTime(2017, 9, 11), 25),
+      new WeatherValue(new DateTime(2017, 9, 12), 23),
+      new WeatherValue(new DateTime(2017, 9, 13), 44),
+      new WeatherValue(new DateTime(2017, 9, 19), 5),
+      new WeatherValue(new DateTime(2017, 9, 20), 25),
+      new WeatherValue(new DateTime(2017, 9, 21), 33),
+      new WeatherValue(new DateTime(2017, 9, 22), 75),
     ];
 
     return [
-      new charts.Series<LinearSales, int>(
-        id: 'Desktop',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (LinearSales sales, _) => sales.year,
-        measureFn: (LinearSales sales, _) => sales.sales,
-        data: myFakeDesktopData,
+      new charts.Series<WeatherValue, DateTime>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.cyan.shadeDefault,
+        domainFn: (WeatherValue sales, _) => sales.time,
+        measureFn: (WeatherValue sales, _) => sales.temp,
+        data: data,
       )
-        // Configure our custom bar target renderer for this series.
-        ..setAttribute(charts.rendererIdKey, 'customArea'),
-      new charts.Series<LinearSales, int>(
-        id: 'Tablet',
-        colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
-        domainFn: (LinearSales sales, _) => sales.year,
-        measureFn: (LinearSales sales, _) => sales.sales,
-        data: myFakeTabletData,
-      ),
     ];
   }
 }
 
-/// Sample linear data type.
-class LinearSales {
-  final int year;
-  final int sales;
+/// Sample time series data type.
+class WeatherValue {
+  final DateTime time;
+  final int temp;
 
-  LinearSales(this.year, this.sales);
+  WeatherValue(this.time, this.temp);
 }
