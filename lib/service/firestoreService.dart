eb8efecc21f5de04df61040290a265b6ae0e7c1c
Josef Wilhelm <josef.wilhelm@bikecitizens.net>
2018-10-23 17:22:01 +0200

    add functions to firestore service

--- lib/service/firestoreService.dart
+++ lib/service/firestoreService.dart
@@ -1,17 +1,65 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'dart:math';
 
 class FirestoreService {
-  Firestore firestore;
+  Firestore firestore = Firestore();
   CollectionReference usersReference;
   CollectionReference stationsReference;
+  FirebaseUser currentUser;
 
-  FirestoreService() {
-    firestore = Firestore.instance;
-    usersReference = firestore.collection("users");
-    stationsReference = firestore.collection("stations");
+  List<DocumentSnapshot> stations;
+
+  FirestoreService(FirebaseUser user) {
+    this.currentUser = user;
+    this.usersReference = firestore.collection("users");
+    this.stationsReference = firestore.collection("stations");
   }
 
-  Future<QuerySnapshot> getAllStations() {
-    return stationsReference.getDocuments();
+  void _loadData() {
+    //TODO
+    _getAllStationsForUser("").then((allStations) {
+      stations.addAll(allStations.documents);
+    });
+  }
+
+  Future<QuerySnapshot> _getAllStationsForUser(String userId) {
+    return stationsReference.where("userId", isEqualTo: userId).getDocuments();
+  }
+
+  void addNewStation(String name, String userId) {
+    stationsReference
+        .document()
+        .setData({'stationName': name, 'userId': userId}).catchError(
+            (e) => debugPrint(e.toString()));
+  }
+
+  //DEBUG seed data to firestore
+  void addDataPeriodically() {
+    Timer.periodic(Duration(minutes: 15), (Timer t) => addData());
+  }
+
+  void addData() {
+    var timestamp = FieldValue.serverTimestamp();
+
+    Random random = Random();
+
+    stations.forEach((station) {
+      station.reference
+          .collection('data')
+          .add({
+            'tempAir': random.nextInt(30) + random.nextDouble(),
+            'precipition': random.nextDouble(),
+            'tempGround': 5 + random.nextInt(20) + random.nextDouble(),
+            'isWet': random.nextInt(20) == 19 ? false : true,
+            'timestamp': timestamp
+          })
+          .then((document) =>
+              debugPrint("written to document: " + document.documentID))
+          .catchError((error) => debugPrint(error.toString()));
+    });
   }
 }
